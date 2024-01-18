52fbec6710a7ce0e292c07c328c68b0c95e15d53
lsw <475967550@qq.com>
2024-01-18 17:34:55 +0800

    test

--- README.md
+++ README.md
@@ -9,12 +9,14 @@
 常见的算法技巧：二分法、排序、双指针、滑动窗口、并查集、深度优先遍历、和广度优先遍历。
 
 ## 时间复杂度分析
+
 ![Screenshot_20240118_143602](https://github.com/shizishen/Algorithm-Notes/assets/85082613/760bb437-5a53-4b1e-aef6-810a72b35dcc)
 
 ![Screenshot_20240118_143951](https://github.com/shizishen/Algorithm-Notes/assets/85082613/2b7b1d28-d0c6-49d9-aa55-a24defce5164)
 
-
 # 第二章：数学之美
+
+
 
 # 第三章：回文的艺术
 
@@ -31,12 +33,6 @@
 # 第九章：双指针
 
 # 第十章：动态规划
-
-
-
-
-
-
 
 # 参考
 
--- 数学之美/twoSum.py
+++ 数学之美/twoSum.py
@@ -1,4 +1,4 @@
-# 解法一
+# 解法一 排序+双指针（不符合）
 def twoSum(nums: list[int], target: int) -> list[int]:
     n = len(nums)
     nums.sort()
@@ -14,13 +14,23 @@ def twoSum(nums: list[int], target: int) -> list[int]:
             return [l, r]
     return []
 
-# 解法二
+
+# 解法二 空间换时间
 def twoSum2(nums: list[int], target: int) -> list[int]:
+    n = len(nums)
+    mapper = {}
+    for i in range(n):
+        if target-nums[i] in mapper:
+            return [mapper[target-nums[i]],i]
+        else:
+            mapper[nums[i]] = i
+    return []
 
 def main():
     nums = [1, 23, 34, 45, 8, 7, 8]
     target = 8
-    index = twoSum(nums, target)
+    #index = twoSum(nums, target)
+    index = twoSum2(nums, target)
     print(index)
 
 
